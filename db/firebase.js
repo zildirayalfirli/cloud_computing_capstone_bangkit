1151094b01d4e0328382fac118484ea71c7dc01d
Zildiray Alfirli <zildirayalfirli@gmail.com>
2024-12-08 07:26:11 +0700

    vercel

--- db/firebase.js
+++ db/firebase.js
@@ -1,16 +1,9 @@
 import admin from "firebase-admin";
-import fs from "fs";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const serviceAccountPath = process.env.FIRESTORE;
-
-if (!serviceAccountPath) {
-  throw new Error("FIRESTORE environment variable is not defined.");
-}
-
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
+const serviceAccount = JSON.parse(process.env.FIRESTORE);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
