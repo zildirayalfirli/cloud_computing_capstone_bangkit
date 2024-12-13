64f9765f2c0e009dc60c72910497c90a92f76d5b
adhitptra <91391759+adhitptra@users.noreply.github.com>
2024-12-13 11:19:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use('/api/expanse', expanseRoutes);
 app.use('/api/wallet', walletRoutes);
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
